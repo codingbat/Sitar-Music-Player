9812305c98142758e21eda8ad2b404549c5c25e2
unknown <hideandseek@protonmail.com>
2015-05-08 00:23:57 +0100

    Project Created

---
+++ src/com/nazmulalam/sitarplayer/MusicController.java
@@ -0,0 +1,19 @@
+package com.nazmulalam.sitarplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/*
+ * http://code.tutsplus.com/tutorials/create-a-music-player-on-android-project-setup--mobile-22764
+ * Sue Smith - February 2014
+ */
+
+public class MusicController extends MediaController {
+
+	public MusicController(Context c){
+		super(c);
+	}
+
+	public void hide(){}
+
+}
